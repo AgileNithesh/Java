379c68bf5a6f4d1d5d511ccdf5b49bb49436e7b8
Nithesh Kumar <nitheshkumar@Nitheshs-MacBook-Air.local>
2025-10-13 01:13:45 +0100

    commit check

--- learn-java/src/main/java/learn/java/concepts/HelloWorld.java
+++ learn-java/src/main/java/learn/java/concepts/HelloWorld.java
@@ -7,6 +7,6 @@ public class HelloWorld {
     public static void main() {
         IO.println("Hello world from Macbook Air!");
         Parrot p = new Parrot();
-        p.arielView();
+        p.arielView(); 
     }
 }
