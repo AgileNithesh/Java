5fd499afce6fcacc0b095a049655d3cb762b4c37
Nithesh Kumar <nitheshkumar852000@gmail.com>
2026-02-02 22:09:53 +0000

    collection framework

--- learn-java/README.md
+++ learn-java/README.md
@@ -4,9 +4,9 @@
 
 ## Access Specifiers
 
-- private -- access from same class only.\
-- public -- access from anywhere.\
-- protected -- access from other class in same package or subclasses in different package\
+- private -- access from same class only.
+- public -- access from anywhere.
+- protected -- access from other class in same package or subclasses in different package
 - default -- if access specifier is not specified then default is used by java, it can be
             accessed only by the classes in same package. 
  
---
+++ learn-java/src/main/java/learn/java/concepts/collections/CollectionFramework.java
@@ -0,0 +1,108 @@
+package learn.java.concepts.collections;
+
+import java.util.*;
+
+public class CollectionFramework {
+
+    /**
+     *
+     *  Collections is group of objects and framework is the generic architecture for different types of
+     *  Objects. Generic method for add, update, delete, search etc..
+     *
+     *  Iterable ->  collection extends itrerable
+     *
+     *  Iterable helps in traversing
+     *
+     *
+     *
+     *  Collection vs Collections
+     *  -- Collections is the utility class provides methods like sort(), min(), swap(), copy(), etc..
+     *  -- Collection
+     *
+     */
+
+    static void main() {
+
+        checkIterationMethods();
+        collectionCheck();
+        collectionsCheck();
+
+    }
+
+    static void collectionsCheck(){
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(27);
+        list.add(22);
+
+        Collections.sort(list);
+        Collections.frequency(list, 2);
+    }
+
+    static void collectionCheck(){
+
+        // Collection interface provides methods to work on group of objects
+        // ex: size(), isEmpty(), toArray(), contains(), iterator(), add(),
+        // clear(), equals(), stream(), parallelStream().. etc
+
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(27);
+        list.add(22);
+
+        list.size();
+
+        list.isEmpty();
+
+        list.contains(1);
+
+        list.remove(2); // remove value in index
+        list.remove(new Integer(22)); // remove value
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.addAll(list); // add one type of collection into another
+
+        stack.containsAll(list);
+        stack.removeAll(list);
+
+        stack.clear();
+        stack.isEmpty();
+    }
+
+
+
+
+    static void checkIterationMethods(){
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(27);
+        list.add(22);
+
+
+        Iterator<Integer> iterator = list.iterator();
+
+        while(iterator.hasNext()){ // check if values exist
+
+            int val = iterator.next(); // return current value
+            IO.println(val);
+
+            if(val == 27){
+                iterator.remove(); // remove current value
+            }
+        }
+
+        // any collection can be iterated through enhanced for or foreach loop
+        IO.println("after removing");
+        for(int a: list){
+            IO.println(a);
+        }
+
+        // foreach method, :: method interface
+        list.forEach(IO::println);
+    }
+
+}
